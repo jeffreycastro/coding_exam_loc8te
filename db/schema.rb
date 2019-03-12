8b49ed8f962f8379ac8bb636b354a0e366c44b66
Jeffrey Castro <castrojeffreym@oaxis.com>
2019-03-12 10:09:16 +0800

    Setup Post scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_020530) do
+ActiveRecord::Schema.define(version: 2019_03_12_020815) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "name"
     t.integer "counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
