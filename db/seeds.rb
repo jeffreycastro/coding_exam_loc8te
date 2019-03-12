adada2ef54e82352f6f43a05993087e4328254b5
Jeffrey Castro <castrojeffreym@oaxis.com>
2019-03-12 12:47:39 +0800

    Add seeds and update table data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |idx|
+  Blog.create(name: "blog#{idx}")
+end
+
+Blog.all.each do |b|
+  100.times do |idx|
+    b.posts.create(name: "post#{idx}")
+  end
+end
+
+Post.all.each do |p|
+  p.created_at = p.created_at - rand(0..6).days
+  p.save
+end
