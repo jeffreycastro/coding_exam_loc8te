aea1fb22c0f3fc388ad8a43fe0720544ef9ffb63
Jeffrey Castro <castrojeffreym@oaxis.com>
2019-03-12 13:36:56 +0800

    Update flash message display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'notice' then "alert alert-info alert-dismissible inner_message"
+    when 'success' then "alert alert-success alert-dismissible inner_message"
+    when 'error', 'alert' then "alert alert-danger alert-dismissible inner_message"
+    when 'warning' then "alert alert-warning alert-dismissible inner_message"
+    end
+  end
 end
