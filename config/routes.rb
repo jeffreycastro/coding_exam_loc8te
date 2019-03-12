fdb6d306371d8a671e1c81ace3adc5f491e85081
Jeffrey Castro <castrojeffreym@oaxis.com>
2019-03-12 10:59:44 +0800

    Setup styling with bootswatch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'dashboards#home'
   resources :posts
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
